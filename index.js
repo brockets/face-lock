ed07cf556257f522ba69862e4a01b8817fa4ad00
kamdz <github@kamdz.pl>
2019-09-14 22:23:11 +0200

    odpalenie tensora

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { app, nativeImage, Tray } = require("electron");
 const lockSystem = require("lock-system");
 const path = require("path");
+const canvas = require("canvas");
+const faceapi = require("face-api.js");
+
+// patch nodejs environment, we need to provide an implementation of
+// HTMLCanvasElement and HTMLImageElement, additionally an implementation
+// of ImageData is required, in case you want to use the MTCNN
+const { Canvas, Image, ImageData } = canvas;
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
 const greenEye = nativeImage.createFromPath(path.join("green.png"));
 const redEye = nativeImage.createFromPath(path.join("red.png"));
@@ -26,4 +34,5 @@ app.on("ready", x => {
   createTray();
 
   setEye(greenEye);
+  console.log(faceapi.nets);
 });
