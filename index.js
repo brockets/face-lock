fd197837cddd2e5a3250ee871a643362c3e1ed18
kamdz <git@kamdz.pl>
2019-09-14 19:28:24 +0200

    czystki

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, nativeImage, Tray, BrowserWindow } = require("electron");
+const { app, nativeImage, Tray } = require("electron");
 const lockSystem = require("lock-system");
 const path = require("path");
 
@@ -7,36 +7,14 @@ const redEye = nativeImage.createFromPath(path.join("red.png"));
 
 const AFK_DELAY = 1000;
 
-let mainWindow = null;
 let tray = null;
 
 function createTray() {
   tray = new Tray(nativeImage.createEmpty());
 }
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    modal: true,
-    width: 430,
-    height: 270,
-    // resizable: false,
-    maximizable: false,
-    minimizable: false,
-    alwaysOnTop: true,
-    center: true,
-    show: false,
-    titleBarStyle: "hidden"
-  });
-  mainWindow.loadFile("index.html");
-
-  mainWindow.on("close", e => {
-    mainWindow.hide();
-    e.preventDefault();
-  });
-}
-
 function setEye(icon) {
-  tray.setTitle("oko ci w dupe");
+  tray.setTitle("a teraz chuj");
   tray.setImage(icon);
 
   tray.on("click", function(event) {
@@ -46,11 +24,7 @@ function setEye(icon) {
 }
 
 app.on("ready", x => {
-  // Hide app dock icon.
   app.dock && app.dock.hide();
-  // Create Renderer Window
-  createWindow();
-  // Create System Tray entry and menu
   createTray();
 
   setEye(greenEye);
