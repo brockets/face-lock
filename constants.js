948d37e95891103e4686fc34454ac823174397d7
kamdz <github@kamdz.pl>
2019-09-15 11:16:42 +0200

    paths fix

--- constants.js
+++ constants.js
@@ -3,18 +3,18 @@ const path = require("path");
 
 module.exports = {
   NUMBER_ICONS: [
-    nativeImage.createFromPath(path.join("icons/0.png")),
-    nativeImage.createFromPath(path.join("icons/1.png")),
-    nativeImage.createFromPath(path.join("icons/2.png")),
-    nativeImage.createFromPath(path.join("icons/3.png")),
-    nativeImage.createFromPath(path.join("icons/4.png")),
-    nativeImage.createFromPath(path.join("icons/5.png")),
-    nativeImage.createFromPath(path.join("icons/6.png")),
-    nativeImage.createFromPath(path.join("icons/7.png")),
-    nativeImage.createFromPath(path.join("icons/8.png")),
-    nativeImage.createFromPath(path.join("icons/9.png"))
+    nativeImage.createFromPath(path.resolve("icons/0.png")),
+    nativeImage.createFromPath(path.resolve("icons/1.png")),
+    nativeImage.createFromPath(path.resolve("icons/2.png")),
+    nativeImage.createFromPath(path.resolve("icons/3.png")),
+    nativeImage.createFromPath(path.resolve("icons/4.png")),
+    nativeImage.createFromPath(path.resolve("icons/5.png")),
+    nativeImage.createFromPath(path.resolve("icons/6.png")),
+    nativeImage.createFromPath(path.resolve("icons/7.png")),
+    nativeImage.createFromPath(path.resolve("icons/8.png")),
+    nativeImage.createFromPath(path.resolve("icons/9.png"))
   ],
-  WATCHER_ICON: nativeImage.createFromPath(path.join("icons/watcher.png")),
-  LOCK_ICON: nativeImage.createFromPath(path.join("icons/lock.png")),
-  SAFE_ICON: nativeImage.createFromPath(path.join("icons/safe.png"))
+  WATCHER_ICON: nativeImage.createFromPath(path.resolve("icons/watcher.png")),
+  LOCK_ICON: nativeImage.createFromPath(path.resolve("icons/lock.png")),
+  SAFE_ICON: nativeImage.createFromPath(path.resolve("icons/safe.png"))
 };
